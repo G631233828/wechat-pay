a8c9a5f0fd57b17b677eae131e9099abbcfe970f
G631233828 <631233828@qq.com>
2019-08-29 21:01:13 +0800

    uodate

--- wechatSites/wechat-app/src/main/java/zhongchiedu/compent/ScheduleTask.java
+++ wechatSites/wechat-app/src/main/java/zhongchiedu/compent/ScheduleTask.java
@@ -1,35 +1,34 @@
 package zhongchiedu.compent;
 
-import java.time.LocalDate;
-
 import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 import zhongchiedu.service.ScheduleCardingStatisticsService;
 
 
 @Slf4j
-public class ScheduleTask implements Job {
+@Component
+public class ScheduleTask  {
 	
 	@Autowired
 	private ScheduleCardingStatisticsService scheduleCardingStatisticsService;
 	
-//	//Test @Scheduled(cron = "0/50 * * * * ?")
-//	@Scheduled(cron = "00 59 23 * * ?")
-//	public void todoSchedule() {
-//	
-//	}
-
-
-	@Override
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		log.info("开始执行定时任务"+LocalDate.now());
-		this.scheduleCardingStatisticsService.autoStatistics();
-		log.info("定时任务执行完毕");
-		
-		
+	@Scheduled(cron = "0/2 * * * * ?")
+	//@Scheduled(cron = "00 59 23 * * ?")
+	public void todoSchedule() {
+		scheduleCardingStatisticsService.autoStatistics();
 	}
+
+
+//	@Override
+//	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+//		log.info("开始执行定时任务"+LocalDate.now());
+//		this.scheduleCardingStatisticsService.autoStatistics();
+//		log.info("定时任务执行完毕");
+//		
+//		
+//	}
 }
